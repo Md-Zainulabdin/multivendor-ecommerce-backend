865dc5ee5492ca90f2c8c99e7516e15b4262ad95
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2024-01-22 23:17:27 +0500

    update user and vendor routes

--- backend/routes/user.routes.mjs
+++ backend/routes/user.routes.mjs
@@ -1,8 +1,7 @@
 import { Router } from "express";
-import { app } from "../app.mjs"
 
-import { registerUser, updateUser } from "../controllers/user.controllers.mjs";
-import { login } from "../controllers/login.controller.mjs";
+import { deleteUser, getAllUsers, getUserById, registerUser, updateUser } from "../controllers/user.controllers.mjs";
+import { loginUser } from "../controllers/login.controller.mjs";
 
 import isAuthenticated from "../middlewares/auth.mjs";
 
@@ -10,11 +9,14 @@ import isAuthenticated from "../middlewares/auth.mjs";
 const router = Router();
 
 router.route('/register').post(registerUser)
-router.route("/login").post(login)
+router.route("/login").post(loginUser)
 
 //secured routes
-app.use(isAuthenticated);
+router.use(isAuthenticated);
 router.route('/update/:id').patch(updateUser);
+router.route('/delete/:id').delete(deleteUser);
+router.route('/:id').get(getUserById);
+router.route('/').get(getAllUsers);
 
 
 
--- backend/routes/vendor.routes.mjs
+++ backend/routes/vendor.routes.mjs
@@ -1,8 +1,8 @@
 import { Router } from "express";
 
-import { registerVendor } from "../controllers/vendor.controllers.mjs";
+import { deleteVendor, getAllVendors, getVendorById, registerVendor } from "../controllers/vendor.controllers.mjs";
 import { updateUser } from "../controllers/user.controllers.mjs";
-import { login } from "../controllers/login.controller.mjs";
+import { loginVendor } from "../controllers/login.controller.mjs";
 
 import isAuthenticated from "../middlewares/auth.mjs";
 import { isVendor } from "../middlewares/isVendor.mjs"
@@ -10,11 +10,14 @@ import { isVendor } from "../middlewares/isVendor.mjs"
 const router = Router();
 
 router.route('/register').post(registerVendor)
-router.route("/login").post(login)
+router.route("/login").post(loginVendor)
 
 //secured routes
-app.use(isAuthenticated);
+router.use(isAuthenticated);
 router.route('/update/:id').patch(updateUser);
+router.route('/delete/:id').delete(deleteVendor);
+router.route('/:id').get(getVendorById);
+router.route('/').get(getAllVendors);
 
 
 export default router;
